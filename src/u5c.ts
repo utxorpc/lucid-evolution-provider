9832216911f7031fe51377e35b4b694496b4e866
johnquinnvictaboada <quinnvictaboada@gmail.com>
2024-09-20 05:36:47 +0800

    feat: Initial U5C provider

---
+++ src/u5c.ts
@@ -0,0 +1,237 @@
+import {
+  Address,
+  Assets,
+  Credential,
+  Datum,
+  DatumHash,
+  Delegation,
+  EvalRedeemer,
+  OutRef,
+  ProtocolParameters,
+  Provider,
+  RewardAddress,
+  Script,
+  Transaction,
+  TxHash,
+  TxOutput,
+  Unit,
+  UTxO,
+} from "@lucid-evolution/core-types";
+import { CardanoQueryClient, CardanoSubmitClient } from "@utxorpc/sdk";
+import * as Cardano from "@utxorpc/spec/lib/utxorpc/v1alpha/cardano/cardano_pb.js";
+
+export class U5C implements Provider {
+  private queryClient: CardanoQueryClient;
+  private submitClient: CardanoSubmitClient;
+
+  constructor({
+    url,
+    headers,
+  }: {
+    url: string;
+    headers?: Record<string, string>;
+  }) {
+    this.queryClient = new CardanoQueryClient({
+      uri: url,
+      headers,
+    });
+
+    this.submitClient = new CardanoSubmitClient({
+      uri: url,
+      headers,
+    });
+  }
+  evaluateTx(
+    tx: Transaction,
+    additionalUTxOs?: UTxO[]
+  ): Promise<EvalRedeemer[]> {
+    throw new Error("Method not implemented.");
+  }
+
+  async getProtocolParameters(): Promise<ProtocolParameters> {
+    const rpcPParams = await this.queryClient.readParams();
+    if (rpcPParams === undefined || rpcPParams === null) {
+      throw new Error(`Error fetching protocol parameters`);
+    }
+    return this._rpcPParamsToCorePParams(rpcPParams);
+  }
+
+  async getUtxos(addressOrCredential: Address | Credential): Promise<UTxO[]> {
+    let addressBytes: Uint8Array;
+
+    if (typeof addressOrCredential === "string") {
+      // TODO: turn address into bytes properly
+      addressBytes = new Uint8Array(Buffer.from(addressOrCredential, "hex"));
+    } else {
+      // TODO: look into Credentials based on other providers
+      throw new Error(`Method not implemented.`);
+    }
+
+    let utxoSearchResult =
+      await this.queryClient.searchUtxosByAddress(addressBytes);
+
+    // TODO: Test mapping
+    return utxoSearchResult.map((result: any) => this._mapToUTxO(result));
+  }
+
+  async getUtxosWithUnit(
+    addressOrCredential: Address | Credential,
+    unit: Unit
+  ): Promise<UTxO[]> {
+    let addressBytes: Uint8Array;
+
+    if (typeof addressOrCredential === "string") {
+      // TODO: turn address into bytes properly
+      addressBytes = new Uint8Array(Buffer.from(addressOrCredential.toString(), "hex"));
+
+    } else {
+      // TODO: look into Credentials based on other providers
+      throw new Error(`Method not implemented.`);
+    }
+
+    const unitBytes = new Uint8Array(Buffer.from(unit, "hex"));
+
+    let utxoSearchResult =
+      await this.queryClient.searchUtxosByAddressWithAsset(
+        addressBytes,
+        undefined,
+        unitBytes
+      );
+
+    // TODO: Test mapping
+    return utxoSearchResult.map((result: any) => this._mapToUTxO(result));
+  }
+
+  async getUtxoByUnit(unit: Unit): Promise<UTxO> {
+    const unitBytes = new Uint8Array(Buffer.from(unit, "hex"));
+  
+    const utxoSearchResult = await this.queryClient.searchUtxosByAsset(
+      undefined,
+      unitBytes
+    );
+  
+    if (utxoSearchResult.length === 0) {
+      throw new Error("No UTxO found for the given unit.");
+    }
+  
+    return this._mapToUTxO(utxoSearchResult[0]);
+  }
+
+  async submitTx(tx: Transaction): Promise<TxHash> {
+    const txBytes = Buffer.from(tx, "hex");
+    const hash = await this.submitClient.submitTx(new Uint8Array(txBytes));
+    return Buffer.from(hash).toString("hex");
+  }
+
+  async getUtxosByOutRef(outRefs: Array<OutRef>): Promise<UTxO[]> {
+    throw new Error("Method not implemented.");
+  }
+
+  async getDelegation(rewardAddress: RewardAddress): Promise<Delegation> {
+    throw new Error("Method not implemented.");
+  }
+
+  async getDatum(datumHash: DatumHash): Promise<Datum> {
+    throw new Error("Method not implemented.");
+  }
+
+  async awaitTx(txHash: TxHash, checkInterval?: number): Promise<boolean> {
+    throw new Error("Method not implemented.");
+  }
+
+  private _mapToUTxO(result: any): UTxO {
+    const txHash: TxHash = result.txHash;
+    const outputIndex: number = result.outputIndex;
+    const address: Address = result.address;
+
+    const assets: Assets = Object.entries(result.assets).reduce(
+      (acc: Assets, [unit, amount]) => {
+        acc[unit] = amount as bigint;
+        return acc;
+      },
+      {}
+    );
+
+    const datumHash: DatumHash | null = result.datumHash ?? null;
+    const datum: Datum | null = result.datum ?? null;
+
+    const scriptRef: Script | null = result.scriptRef
+      ? {
+          type: "Native", 
+          script: result.scriptRef,
+        }
+      : null;
+
+    const outRef: OutRef = {
+      txHash,
+      outputIndex,
+    };
+
+    const txOutput: TxOutput = {
+      address,
+      assets,
+      datumHash,
+      datum,
+      scriptRef,
+    };
+
+    // Combine them into UTxO
+    return {
+      ...outRef,
+      ...txOutput,
+    };
+  }
+
+  private _rpcPParamsToCorePParams(
+    rpcPParams: Cardano.PParams
+  ): ProtocolParameters {
+    return {
+      minFeeA: Number(rpcPParams.minFeeCoefficient),
+      minFeeB: Number(rpcPParams.minFeeConstant),
+      maxTxSize: Number(rpcPParams.maxTxSize),
+      maxValSize: Number(rpcPParams.maxValueSize),
+      keyDeposit: BigInt(rpcPParams.stakeKeyDeposit),
+      poolDeposit: BigInt(rpcPParams.poolDeposit),
+      drepDeposit: BigInt(0), // TODO: find values
+      govActionDeposit: BigInt(0), // TODO: find values
+      priceMem: Number(rpcPParams.prices?.memory),
+      priceStep: Number(rpcPParams.prices?.steps),
+      maxTxExMem: BigInt(
+        rpcPParams.maxExecutionUnitsPerTransaction?.memory || 0
+      ),
+      maxTxExSteps: BigInt(
+        rpcPParams.maxExecutionUnitsPerTransaction?.steps || 0
+      ),
+      coinsPerUtxoByte: BigInt(rpcPParams.coinsPerUtxoByte),
+      collateralPercentage: Number(rpcPParams.collateralPercentage),
+      maxCollateralInputs: Number(rpcPParams.maxCollateralInputs),
+      minFeeRefScriptCostPerByte: 0, // TODO: find values
+      // TODO: find values
+      costModels: {
+        PlutusV1: rpcPParams.costModels?.plutusV1?.values.reduce(
+          (model: Record<string, number>, value: any, index: number) => {
+            model[index.toString()] = Number(value.toString());
+            return model;
+          },
+          {}
+        ) ?? {},
+      
+        PlutusV2: rpcPParams.costModels?.plutusV2?.values.reduce(
+          (model: Record<string, number>, value: any, index: number) => {
+            model[index.toString()] = Number(value.toString());
+            return model;
+          },
+          {}
+        ) ?? {},
+      
+        PlutusV3: rpcPParams.costModels?.plutusV3?.values.reduce(
+          (model: Record<string, number>, value: any, index: number) => {
+            model[index.toString()] = Number(value.toString());
+            return model;
+          },
+          {}
+        ) ?? {},
+      }
+    };
+  }
+}
